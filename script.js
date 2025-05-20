8b8d0f549791ceab7ab1c9b16494bcdd47b953be
gatormeek <204446387+gatormeek@users.noreply.github.com>
2025-05-19 20:56:17 -0700

    Gets playRound function working. This gets the playRound function to work, still need to add styles and game end mechanics.

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
-let getComputerChoice = function(arr) {
-    arr=["ROCK","PAPER","SCISSORS"];
-    const randomNum = [Math.floor(Math.random()*arr.length)];
-    return arr[randomNum];
+let playerSelection;
+
+const decisionContainer=document.querySelector('#decision-container');
+decisionContainer.addEventListener('click', (event) => {
+    if (event.target.id==='ROCK') {
+        playerSelection=event.target.id;
+        document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
+        console.log(`Button clicked, player Selection defined as: ${playerSelection}`);
+        playRound(playerSelection);
+    } if (event.target.id==='PAPER') {
+        playerSelection=event.target.id;
+        document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
+        console.log(`Button clicked, player selection defined as: ${playerSelection}`);
+        playRound(playerSelection);
+    } if (event.target.id==='SCISSORS') {
+        playerSelection=event.target.id;
+        document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
+        console.log(`Button clicked, player selection defined as: ${playerSelection}`);
+        playRound(playerSelection);
+    }
+});
+const choices = ["ROCK", "PAPER", "SCISSORS"];    
+let getComputerChoice = function() {
+    return choices[Math.floor(Math.random()*choices.length)];
 }
-
- //get rid of this once you have a playRound function
-
-let getChoices = function(userChoice,computerChoice) {
-    rock.onclick = () => {
-        userChoice = "ROCK";
-        console.log(`You chose: ${userChoice}`);
-        computerChoice = getComputerChoice();
-        console.log(`Computer chose: ${computerChoice}`);
-    }
-    paper.onclick = () => {
-        userChoice = "PAPER";
-        console.log(`You chose ${userChoice}`);
-        computerChoice = getComputerChoice();
-        console.log(`Computer chose: ${computerChoice}`);
-    }
-    scissors.onclick = () => {
-        userChoice = "SCISSORS";
-        console.log(`You chose ${userChoice}`);
-        computerChoice = getComputerChoice();
-        console.log(`Computer chose: ${computerChoice}`);
-    } 
+let userScore=0;
+let computerScore=0;
+function playRound(playerSelection) {
     
- }
+    console.log(`Player Selection: ${playerSelection}`);
+    const computerSelection=getComputerChoice();
+    console.log(`Computer Selection: ${computerSelection}`);
+    document.querySelector('#computer-play').textContent=`Computer Chose: ${computerSelection}`;
+        let result;
+        if (playerSelection===computerSelection) {
+            result='Tie';
+        } else if ((playerSelection==='ROCK' && computerSelection==='SCISSORS') || 
+        (playerSelection==='PAPER' && computerSelection==='ROCK') ||
+        (playerSelection==='SCISSORS' && computerSelection==='PAPER')) {
+            result='You win';
+            userScore++;
+        } else {
+            result='You lose';
+            computerScore++;
+        }
+        document.querySelector('#result').textContent=`${result}`;
+        document.querySelector('#computer-score').textContent=`Computer Score: ${computerScore}`;
+        document.querySelector('#user-score').textContent=`Your Score: ${userScore}`;
+}
 
-rock.addEventListener("click", getChoices());
-paper.addEventListener("click", getChoices());
-scissors.addEventListener("click", getChoices());
-
-let playRound = function() {
-    if (getChoices("PAPER","PAPER")) {
-        console.log("tie");
-    } else if (getChoices("ROCK","ROCK")) {
-        console.log("tie");
-    } else if (getChoices("SCISSORS","SCISSORS")) {
-        console.log("tie");
+let checkGameComplete = function() {
+    if (userScore===5 || computerScore===5) {
+        decisionContainer.foreach((btn)=>(btn.disabled=true));
+        if (userScore>computerScore) {
+            document.querySelector('#game-result').textContent="You win!";
+        } else {
+            document.querySelector('#game-result').textContent="You lose!";
+        }
     }
 }
-playRound();
+
+
+
+
