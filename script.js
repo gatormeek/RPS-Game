dfd486a0d1e1fc8912d5f9a11772c3f785f8cb9a
gatormeek <204446387+gatormeek@users.noreply.github.com>
2025-05-20 22:12:10 -0700

    add styles, set game functionality
    this will add styles to the user experience, as
    well as full game functionality. Finishing styles
    on next commit.

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 let playerSelection;
-
+const gameContainer=document.querySelector('#game-container');
 const decisionContainer=document.querySelector('#decision-container');
+const restartGame=document.querySelector('#restart-game');
 decisionContainer.addEventListener('click', (event) => {
     if (event.target.id==='ROCK') {
         playerSelection=event.target.id;
         document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
-        console.log(`Button clicked, player Selection defined as: ${playerSelection}`);
         playRound(playerSelection);
     } if (event.target.id==='PAPER') {
         playerSelection=event.target.id;
         document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
-        console.log(`Button clicked, player selection defined as: ${playerSelection}`);
         playRound(playerSelection);
     } if (event.target.id==='SCISSORS') {
         playerSelection=event.target.id;
         document.querySelector('#user-play').textContent=`You chose: ${playerSelection}`;
-        console.log(`Button clicked, player selection defined as: ${playerSelection}`);
         playRound(playerSelection);
     }
 });
@@ -25,23 +23,32 @@ let getComputerChoice = function() {
 }
 let userScore=0;
 let computerScore=0;
+let ties=0;
 function playRound(playerSelection) {
-    
-    console.log(`Player Selection: ${playerSelection}`);
     const computerSelection=getComputerChoice();
-    console.log(`Computer Selection: ${computerSelection}`);
     document.querySelector('#computer-play').textContent=`Computer Chose: ${computerSelection}`;
         let result;
         if (playerSelection===computerSelection) {
             result='Tie';
+            ties++;
+            if (ties===1) {
+            document.querySelector('#ties').textContent=`You tied.`
+            } else if (ties===2) {
+            document.querySelector('#ties').textContent=`Ties: ${ties}. You idiot. You've tied twice. Do better.`
+            } else if (ties>2) {
+            document.querySelector('#ties').textContent=`Ties: ${ties}. Are you even trying?`
+            }
         } else if ((playerSelection==='ROCK' && computerSelection==='SCISSORS') || 
         (playerSelection==='PAPER' && computerSelection==='ROCK') ||
         (playerSelection==='SCISSORS' && computerSelection==='PAPER')) {
             result='You win';
             userScore++;
+            checkGameComplete();
+
         } else {
             result='You lose';
             computerScore++;
+            checkGameComplete();
         }
         document.querySelector('#result').textContent=`${result}`;
         document.querySelector('#computer-score').textContent=`Computer Score: ${computerScore}`;
@@ -50,15 +57,16 @@ function playRound(playerSelection) {
 
 let checkGameComplete = function() {
     if (userScore===5 || computerScore===5) {
-        decisionContainer.foreach((btn)=>(btn.disabled=true));
+        gameContainer.style.display = 'none';
         if (userScore>computerScore) {
-            document.querySelector('#game-result').textContent="You win!";
+            return document.querySelector('#game-result').textContent="You win!";
         } else {
-            document.querySelector('#game-result').textContent="You lose!";
+            return document.querySelector('#game-result').textContent="You lose!";
         }
     }
 }
 
-
-
-
+let restart = function (userScore, computerScore) {
+    userScore=0;
+    computerScore=0;
+}
